1c449f4ead2224ac95dc8a83e5cdd39ffde6164d
arokosandria <kmsa@interia.pl>
2019-05-17 00:10:43 +0200

    Add Test

--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.mwo;
 import static org.junit.Assert.*;
 
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,12 +9,18 @@ import org.junit.Test;
 public class TripManagerTest {
 
 	TripManager tripManager;
-	Trip trip;
+	Trip trip, trip1, trip2, trip3;
+	Photo photo;
 	
 	@Before
 	public void setUp() {
 		tripManager = new TripManager();
 		trip = new Trip("nazwa", "opis");
+		trip1 = new Trip("ekstra nazwa wycieczki", "Teneryfa Last Minute");
+		trip2 = new Trip("Super nazwa wycieczki", "Egipt all incusive");
+		trip3 = new Trip("Extra promocja", "Tunezja");
+		photo=new Photo("piekne wybrzeze");
+		trip3.addPhoto(photo);
 	}
 	
 	@Test
@@ -37,6 +44,32 @@ public class TripManagerTest {
 		assertEquals(1, tripManager.getTrips().size());
 		tripManager.remove(trip.getName());
 		assertEquals(0, tripManager.getTrips().size());
-		fail("chcemy zespuc");
+		//fail("chcemy zespuc");
 		}
+	@Test
+    public void testFindTripIsNull() throws TripAlreadyExistsException {
+        tripManager.add(trip);
+        tripManager.add(trip1);
+        tripManager.add(trip2);
+        tripManager.add(trip3);
+        assertEquals(4,tripManager.findTrip("").size());
+}
+	@Test
+	public void testFindTripName() throws TripAlreadyExistsException {
+		tripManager.add(trip1);
+		tripManager.add(trip2);
+		assertEquals(2, tripManager.findTrip("wycieczki").size());
+	}
+
+	@Test
+	public void testFindDesctiption() throws TripAlreadyExistsException {
+		tripManager.add(trip3);
+		assertEquals(1, tripManager.findTrip("Tunezja").size());
+	}
+
+	@Test
+	public void testFindPhoto() throws TripAlreadyExistsException {
+		tripManager.add(trip3);
+		assertEquals(1, tripManager.findTrip("piekne wybrzeze").size());
+}
 }
