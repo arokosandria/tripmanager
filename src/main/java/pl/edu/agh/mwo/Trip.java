5f642c00b8e73d1d56b43e3fdddb97c542a99a08
arokosandria <kmsa@interia.pl>
2019-05-17 00:09:48 +0200

    Create Trip class

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -1,12 +1,17 @@
 package pl.edu.agh.mwo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Trip {
 	private String name;
 	private String description;
+	private List<Photo> photon;
 	
 	public Trip(String name, String description) {
 		this.name = name;
 		this.description = description;
+		photon = new ArrayList<Photo>();
 	}
 
 	public String getName() {
@@ -24,4 +29,11 @@ public class Trip {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	public void addPhoto(Photo photo) {
+		photon.add(photo);
+	}
+
+	public List<Photo> getPhotos() {
+		return photon;
+}
 }
