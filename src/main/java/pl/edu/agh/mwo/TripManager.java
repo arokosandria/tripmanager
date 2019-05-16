c853d27c94a4af75f0aba1ff36ce11b52830078d
arokosandria <kmsa@interia.pl>
2019-05-17 00:10:18 +0200

    Edit TripManager Class

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -24,5 +24,29 @@ public class TripManager {
 	public void remove(String name) {
 		tripList.remove(name);
 	}
+
+public Map<String,Trip> findTrip(String keyword){
+	HashMap<String, Trip> foundTrips=new HashMap();
+	if(keyword=="") {
+			foundTrips=tripList;
+	}
+	for (String nametrip : tripList.keySet()) {
+        if (nametrip.contains(keyword))
+            foundTrips.put(nametrip,tripList.get(nametrip));
 	
+	else if (tripList.get(nametrip).getDescription().contains(keyword)) {
+		foundTrips.put(nametrip,tripList.get(nametrip));
+	} else {
+		List<Photo> photos = tripList.get(nametrip).getPhotos();
+		for (Photo photo : photos) {
+			if (photo.getComment().contains(keyword)) {
+				foundTrips.put(nametrip,tripList.get(nametrip));;
+			}
+		}
+	}
+}
+	return foundTrips;
+	
+		
+}
 }
